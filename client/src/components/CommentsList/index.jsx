c5773ff9bde2a4fd8e904d2252cb021df4bc8413
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-09-30 11:22:19 +0300

    feat: client, comment status display styling

--- client/src/components/CommentsList/index.jsx
+++ client/src/components/CommentsList/index.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 
+const COLOR = Object.freeze({
+  approved: "green",
+  rejected: "red",
+  pending: "blue",
+});
+
 /**
  * Description
- * @param {{comments: { id: string; content: string; status: string; }[]}} props
+ * @param {{comments: { id: string; content: string; status: 'approved'|'pending'|'rejected'; }[]}} props
  * @returns {any}
  */
 const CommentsList = ({ comments }) => {
   return (
     <ul className=" list-disc pl-4 my-2 max-h-16 overflow-y-scroll">
-      {comments.map((comment) => (
-        <li key={comment.id}>
-          {comment.content} <small className="text-xs">{comment.status}</small>
+      {comments.map(({ id, content, status }) => (
+        <li key={id}>
+          {content}{" "}
+          <small
+            className={`text-xs font-semibold text-${COLOR[status]}-600 px-1 border border-${COLOR[status]}-600 rounded`}
+          >
+            {status}
+          </small>
         </li>
       ))}
     </ul>
